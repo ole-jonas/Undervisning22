04544bd7118c89569952c71b038da0d24e55ffcd
ole-jonas <olejonaskoder@gmail.com>
2024-12-12 13:57:03 +0100

    Css

--- script.js
+++ script.js
@@ -1,44 +1,4 @@
-setTimeout(
-    function () {
-        console.log("Noe har skjedd!");
-    }, 
-    10000
-);
-
-//
-//
-
-function timeout1 () {
-    console.log("Noe skal skje");
-
-    setTimeout (function () {
-        console.log("Noe har skjedd");
-    }, 10000)
-}
-
-timeout1();
-
-//
-//
-
-function delay(ms) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, ms);
-    });
-};
-
-async function timeout2 () {
-    console.log("Noe kommer til å skje");
-    await delay(5000);
-    console.log("Noe har skjedd!");
-}
-
-timeout2();
-
-//
-//
-
-const myCatFacts = ["Cats are the best", "Cats are just small lions"];
+  const myCatFacts = ["Cats are the best", "Cats are just small lions"];
 
 console.log(myCatFacts[0]);
 
